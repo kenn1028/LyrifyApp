9417109df6bf0e0424a1d8575f3021cba69bae5a
Kenn Pelayo <kenn1028@gmail.com>
2020-06-12 00:18:55 +0800

    SpotifyClient - 90% DONE
    Authentication Process & Scopes - DONE
    
    Functions Finished:
    1.)  get_current_song()
    2.) get_user_playlists(user_id)
    3.) get_current_playlists()
    4.) get_playlist_tracks(playlist_id)
    5.) search functions - incomplete/not updated
    
    Pending:
    6.) get_user_profile(user_id) - returns user's display name, and etc.

---
+++ playlist_json_dict.py
@@ -0,0 +1,352 @@
+data = {
+    "href": "https://api.spotify.com/v1/users/uy6wcttkt4ycnqhe7gynrl3rq/playlists?offset=0&limit=20",
+    "items": [
+        {
+            "collaborative": False,
+            "description": "",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/5ulf2uxcgPauTFmtcAZLDN"
+            },
+            "href": "https://api.spotify.com/v1/playlists/5ulf2uxcgPauTFmtcAZLDN",
+            "id": "5ulf2uxcgPauTFmtcAZLDN",
+            "images": [
+                {
+                    "height": 640,
+                    "url": "https://mosaic.scdn.co/640/ab67616d0000b273709471962e8d18f406f9af70ab67616d0000b2738c2527a1a9fc86aa448aae82ab67616d0000b27395b1203dcedf5044a81a058cab67616d0000b273a5c7cffe137c33ecf5d8c1e2",
+                    "width": 640
+                },
+                {
+                    "height": 300,
+                    "url": "https://mosaic.scdn.co/300/ab67616d0000b273709471962e8d18f406f9af70ab67616d0000b2738c2527a1a9fc86aa448aae82ab67616d0000b27395b1203dcedf5044a81a058cab67616d0000b273a5c7cffe137c33ecf5d8c1e2",
+                    "width": 300
+                },
+                {
+                    "height": 60,
+                    "url": "https://mosaic.scdn.co/60/ab67616d0000b273709471962e8d18f406f9af70ab67616d0000b2738c2527a1a9fc86aa448aae82ab67616d0000b27395b1203dcedf5044a81a058cab67616d0000b273a5c7cffe137c33ecf5d8c1e2",
+                    "width": 60
+                }
+            ],
+            "name": "LL Aqours",
+            "owner": {
+                "display_name": "Mizushima Ken",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/uy6wcttkt4ycnqhe7gynrl3rq"
+                },
+                "href": "https://api.spotify.com/v1/users/uy6wcttkt4ycnqhe7gynrl3rq",
+                "id": "uy6wcttkt4ycnqhe7gynrl3rq",
+                "type": "user",
+                "uri": "spotify:user:uy6wcttkt4ycnqhe7gynrl3rq"
+            },
+            "primary_color": None,
+            "public": True,
+            "snapshot_id": "MTIsOTAxOTBkZTcxM2Q4YmM0NzNlNzY1OTgyNzdkZTU2Njk5NTBmN2Q4OQ==",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/5ulf2uxcgPauTFmtcAZLDN/tracks",
+                "total": 29
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:5ulf2uxcgPauTFmtcAZLDN"
+        },
+        {
+            "collaborative": False,
+            "description": "Tracks from Korea's thriving indie scene.",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/37i9dQZF1DXdTb8AG95jne"
+            },
+            "href": "https://api.spotify.com/v1/playlists/37i9dQZF1DXdTb8AG95jne",
+            "id": "37i9dQZF1DXdTb8AG95jne",
+            "images": [
+                {
+                    "height": None,
+                    "url": "https://i.scdn.co/image/ab67706f00000002d033e85098affe29eb604aeb",
+                    "width": None
+                }
+            ],
+            "name": "K-Indie Picks",
+            "owner": {
+                "display_name": "Spotify",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/spotify"
+                },
+                "href": "https://api.spotify.com/v1/users/spotify",
+                "id": "spotify",
+                "type": "user",
+                "uri": "spotify:user:spotify"
+            },
+            "primary_color": None,
+            "public": False,
+            "snapshot_id": "MTU5MTcyMDc1NywwMDAwMDAwMGQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0Mjdl",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/37i9dQZF1DXdTb8AG95jne/tracks",
+                "total": 50
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:37i9dQZF1DXdTb8AG95jne"
+        },
+        {
+            "collaborative": False,
+            "description": "Mix of Tatsuro Yamashita, Makoto Matsushita, Toshiki Kadomatsu, Mariya Takeuchi, Anri, Junko Ohashi, Taeko Onuki, Miki Matsubara, Omega Tribe, Kiyotaka Sugiyama and Many More! Will be updated monthly",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/7LbezrthyZnAib6SbbPL7V"
+            },
+            "href": "https://api.spotify.com/v1/playlists/7LbezrthyZnAib6SbbPL7V",
+            "id": "7LbezrthyZnAib6SbbPL7V",
+            "images": [
+                {
+                    "height": None,
+                    "url": "https://i.scdn.co/image/ab67706c0000da84142bcce5fd02ae6300b20c12",
+                    "width": None
+                }
+            ],
+            "name": "Japanese City Pop",
+            "owner": {
+                "display_name": "rjananda20",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/rjananda20"
+                },
+                "href": "https://api.spotify.com/v1/users/rjananda20",
+                "id": "rjananda20",
+                "type": "user",
+                "uri": "spotify:user:rjananda20"
+            },
+            "primary_color": None,
+            "public": True,
+            "snapshot_id": "MzQ5LGU4ODM1YmM2YTE1OTdlZjAzZDJhNGRlMDEzZTY4ZTAxOTJiM2U4YzY=",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/7LbezrthyZnAib6SbbPL7V/tracks",
+                "total": 187
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:7LbezrthyZnAib6SbbPL7V"
+        },
+        {
+            "collaborative": False,
+            "description": "Eighties in Japan was a beautiful time. Here take some japanese city pop, funk, future funk, boogie, modern soul, remixes and some old great stuff",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/4x2UJm2TpYcciuws1Mxj8L"
+            },
+            "href": "https://api.spotify.com/v1/playlists/4x2UJm2TpYcciuws1Mxj8L",
+            "id": "4x2UJm2TpYcciuws1Mxj8L",
+            "images": [
+                {
+                    "height": None,
+                    "url": "https://i.scdn.co/image/ab67706c0000da8498feb648dd21a44836f29e73",
+                    "width": None
+                }
+            ],
+            "name": "Japanese City Pop",
+            "owner": {
+                "display_name": "Marta",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/xrzt3qn7tzyq5wpccynmfwf32"
+                },
+                "href": "https://api.spotify.com/v1/users/xrzt3qn7tzyq5wpccynmfwf32",
+                "id": "xrzt3qn7tzyq5wpccynmfwf32",
+                "type": "user",
+                "uri": "spotify:user:xrzt3qn7tzyq5wpccynmfwf32"
+            },
+            "primary_color": None,
+            "public": True,
+            "snapshot_id": "MzI4LGJhNGQ2ZDFmYmEwNzRlZmRkMDFlMjFlNTY1OTA3OTZiMjUyNjZkYTM=",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/4x2UJm2TpYcciuws1Mxj8L/tracks",
+                "total": 307
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:4x2UJm2TpYcciuws1Mxj8L"
+        },
+        {
+            "collaborative": False,
+            "description": "the chillest japanese songs around. includes: alternative, singer&#x2F;songwriter, light pop, and soft rock jamz.",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/1KYCoW1hsKedCnKEF4RvVY"
+            },
+            "href": "https://api.spotify.com/v1/playlists/1KYCoW1hsKedCnKEF4RvVY",
+            "id": "1KYCoW1hsKedCnKEF4RvVY",
+            "images": [
+                {
+                    "height": None,
+                    "url": "https://i.scdn.co/image/ab67706c0000da84cc2f3aa30ac9fa6a41cb91cc",
+                    "width": None
+                }
+            ],
+            "name": "chill japanese vibes",
+            "owner": {
+                "display_name": "bond.brianna",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/bond.brianna"
+                },
+                "href": "https://api.spotify.com/v1/users/bond.brianna",
+                "id": "bond.brianna",
+                "type": "user",
+                "uri": "spotify:user:bond.brianna"
+            },
+            "primary_color": None,
+            "public": True,
+            "snapshot_id": "NzM0LGJmYTUxMDM0NDg1OTc1NTJjZWNiM2MzMTJlNTQ4YjE4ZTg5NTEzNWM=",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/1KYCoW1hsKedCnKEF4RvVY/tracks",
+                "total": 661
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:1KYCoW1hsKedCnKEF4RvVY"
+        },
+        {
+            "collaborative": False,
+            "description": "",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/6aneMCMSIzBNUWTqkgf4uk"
+            },
+            "href": "https://api.spotify.com/v1/playlists/6aneMCMSIzBNUWTqkgf4uk",
+            "id": "6aneMCMSIzBNUWTqkgf4uk",
+            "images": [
+                {
+                    "height": None,
+                    "url": "https://i.scdn.co/image/ab67706c0000da84dc60b7db78402e3d265d9a9d",
+                    "width": None
+                }
+            ],
+            "name": "[KPOP] STAN LIST ",
+            "owner": {
+                "display_name": "Mizushima Ken",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/uy6wcttkt4ycnqhe7gynrl3rq"
+                },
+                "href": "https://api.spotify.com/v1/users/uy6wcttkt4ycnqhe7gynrl3rq",
+                "id": "uy6wcttkt4ycnqhe7gynrl3rq",
+                "type": "user",
+                "uri": "spotify:user:uy6wcttkt4ycnqhe7gynrl3rq"
+            },
+            "primary_color": None,
+            "public": True,
+            "snapshot_id": "Mzc0LGM2NmU3NThkMWIzMTMxY2QxY2IzZDU1OGY5OTc5OWRiMmQxMDllNjU=",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/6aneMCMSIzBNUWTqkgf4uk/tracks",
+                "total": 282
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:6aneMCMSIzBNUWTqkgf4uk"
+        },
+        {
+            "collaborative": False,
+            "description": "O__O",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/5r2W7NH7bIRGs8sAq348hl"
+            },
+            "href": "https://api.spotify.com/v1/playlists/5r2W7NH7bIRGs8sAq348hl",
+            "id": "5r2W7NH7bIRGs8sAq348hl",
+            "images": [
+                {
+                    "height": None,
+                    "url": "https://i.scdn.co/image/ab67706c0000da84ec39b7e382416bc29af6f7a8",
+                    "width": None
+                }
+            ],
+            "name": "focus :)",
+            "owner": {
+                "display_name": "Hannah Mariah Pelayo",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/22naatgpxmhdtot4kfgnjfmki"
+                },
+                "href": "https://api.spotify.com/v1/users/22naatgpxmhdtot4kfgnjfmki",
+                "id": "22naatgpxmhdtot4kfgnjfmki",
+                "type": "user",
+                "uri": "spotify:user:22naatgpxmhdtot4kfgnjfmki"
+            },
+            "primary_color": None,
+            "public": False,
+            "snapshot_id": "MjMsMzllOGQxZjQwYWY3OTU1NWM3NDQzOWQxNmQzNzVhOTkxYjkwZmI0OA==",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/5r2W7NH7bIRGs8sAq348hl/tracks",
+                "total": 636
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:5r2W7NH7bIRGs8sAq348hl"
+        },
+        {
+            "collaborative": False,
+            "description": "",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/13Y6dl6F2USjAmgMs7d763"
+            },
+            "href": "https://api.spotify.com/v1/playlists/13Y6dl6F2USjAmgMs7d763",
+            "id": "13Y6dl6F2USjAmgMs7d763",
+            "images": [
+                {
+                    "height": 640,
+                    "url": "https://i.scdn.co/image/ab67616d0000b27313fa1b29c6c48aaf925d3ae1",
+                    "width": 640
+                }
+            ],
+            "name": "#",
+            "owner": {
+                "display_name": "Mizushima Ken",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/uy6wcttkt4ycnqhe7gynrl3rq"
+                },
+                "href": "https://api.spotify.com/v1/users/uy6wcttkt4ycnqhe7gynrl3rq",
+                "id": "uy6wcttkt4ycnqhe7gynrl3rq",
+                "type": "user",
+                "uri": "spotify:user:uy6wcttkt4ycnqhe7gynrl3rq"
+            },
+            "primary_color": None,
+            "public": True,
+            "snapshot_id": "MyxjYjg1ZGJlZTc2NWE2NTA0NmRkNTk5ZWNlNzFjOTJiMmYxYzJmYzk3",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/13Y6dl6F2USjAmgMs7d763/tracks",
+                "total": 7
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:13Y6dl6F2USjAmgMs7d763"
+        },
+        {
+            "collaborative": False,
+            "description": "Ready for some sweet harmony or explosive combination of your favourite couple or troublemakers.",
+            "external_urls": {
+                "spotify": "https://open.spotify.com/playlist/37i9dQZF1DWZYjbSZYSpu6"
+            },
+            "href": "https://api.spotify.com/v1/playlists/37i9dQZF1DWZYjbSZYSpu6",
+            "id": "37i9dQZF1DWZYjbSZYSpu6",
+            "images": [
+                {
+                    "height": None,
+                    "url": "https://i.scdn.co/image/ab67706f00000002cd3717f8e2b27f8cc2114f99",
+                    "width": None
+                }
+            ],
+            "name": "K-Pop Duets",
+            "owner": {
+                "display_name": "Spotify",
+                "external_urls": {
+                    "spotify": "https://open.spotify.com/user/spotify"
+                },
+                "href": "https://api.spotify.com/v1/users/spotify",
+                "id": "spotify",
+                "type": "user",
+                "uri": "spotify:user:spotify"
+            },
+            "primary_color": None,
+            "public": False,
+            "snapshot_id": "MTU5MTcyMDc1NywwMDAwMDAwMGQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0Mjdl",
+            "tracks": {
+                "href": "https://api.spotify.com/v1/playlists/37i9dQZF1DWZYjbSZYSpu6/tracks",
+                "total": 53
+            },
+            "type": "playlist",
+            "uri": "spotify:playlist:37i9dQZF1DWZYjbSZYSpu6"
+        }
+    ],
+    "limit": 20,
+    "next": None,
+    "offset": 0,
+    "previous": None,
+    "total": 10
+}
+
+list = []
+for i in range((len(data))+1):
+    list.append({"name": data['items'][i]['name'], "playlist_id": data['items'][i]['id'] })
+
+# list = list.decode('utf-8')
+for i in list:
+    print(i)
